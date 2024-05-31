c8a2b0e1714cb44ed2cb86204d87e080f9a03f07
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-05-31 14:02:37 +0100

    Moved the handleImageRotation function outside the forEach loop for clearer separation

--- static/js/script.js
+++ static/js/script.js
@@ -15,36 +15,34 @@ boxes.forEach((box) => {
         box.addEventListener(MOUSEOUT, (e) => handleImagesRotation(e.type, images));
     }
    
-    function handleImagesRotation(type, images) {
-
-        const [img, img2]           = images;
-        const DELAY_IN_MILLISECONDS = 400;
-
-        switch (true) {
-
-            case type === "mouseover":
-                img.style.transform = "rotate(180deg)";
-
-                setTimeout(() => {
-                    img.style.display  = "none";
-                    img2.style.display = "block";
-                }, DELAY_IN_MILLISECONDS);
-
-                break;
-            
-            case type === "mouseout":
-                img.style.transform = "rotate(360deg)";
-                
-                setTimeout(() => {
-                    img.style.display = "block";
-                    img2.style.display = "none";
-                }, DELAY_IN_MILLISECONDS);
-                break;
-        }
-       
-    }
-
-   
 })
 
 
+function handleImagesRotation(type, images) {
+
+    const [img, img2]           = images;
+    const DELAY_IN_MILLISECONDS = 400;
+
+    switch (true) {
+
+        case type === "mouseover":
+            img.style.transform = "rotate(180deg)";
+
+            setTimeout(() => {
+                img.style.display  = "none";
+                img2.style.display = "block";
+            }, DELAY_IN_MILLISECONDS);
+
+            break;
+        
+        case type === "mouseout":
+            img.style.transform = "rotate(360deg)";
+            
+            setTimeout(() => {
+                img.style.display = "block";
+                img2.style.display = "none";
+            }, DELAY_IN_MILLISECONDS);
+            break;
+    }
+   
+}
