aeb32c12ebb9d8f9c0dcbbf39e6a9ecdf4bd88c9
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-07-29 16:32:33 +0100

    feat: Add navigation functionality for form pages and create input fields for new pages
    Navigation path:
      - Home > My account > Product management overview > Add a new product
    
    Added form attributes for the following pages:
      - image-and-media.html
      - SEO-and-meta-information.html
      - shipping-and-delivery.html
      - additional-information.html
    
    Updated the textarea to display character count for the following pages:
      - basic-product-information.html
      - detailed-description-specs.html
      - SEO-and-meta-information.html
    
    Additional changes:
      - Created form attributes for the above pages (e.g., input fields, select fields, etc.).
      - Prevented default form submission behavior to allow for custom validation and navigation logic.
      - Added a character counter to textarea descriptions to show the user the character count.
      - Added a `current page` indicator in the progress list to show the user their current page.
      - Enabled the user to select a country from the given country fields.
    
    TODO: Add form functionalities for the following page:
      - review-and-submit.html
    
    Note:
      - The app currently does not capture user form data. Data capture will be handled after all form inputs for the pages listed in the TODO section are built.

--- static/js/components/add-product.js
+++ static/js/components/add-product.js
@@ -1,5 +1,61 @@
 import addNewProductPages from "../pages/pages.js";
 import { redirectToNewPage } from "../utils/utils.js";
+import { minimumCharactersToUse } from "./characterCounter.js";
+import loadFile from "../utils/loader.js";
+
+
+const detailDescriptionTextAreaSelector   = "#detailed-description";
+const shortDescriptionTextAreaSelector    = "#short-description";
+const metaDescriptionTextAreaSelector     = "#meta-description";
+const warrantyDescriptionTextAreaSelector = "#warranty-description";
+
+
+// Text area field inside inside the detail description specs page
+minimumCharactersToUse(detailDescriptionTextAreaSelector, {
+                        minCharClass: '.minimum-characters',
+                        minCharMessage: 'Minimum characters to use: ',
+                        maxCharClass: '.maximum-characters',
+                        maxCharMessage: 'Number of characters remaining: ',
+                        minCharsLimit: 50,
+                        maxCharsLimit: 1000
+                        });
+
+
+
+// Text area field for the basic product information page
+minimumCharactersToUse(shortDescriptionTextAreaSelector, {
+                                minCharClass: '.minimum-characters',
+                                minCharMessage:'Minimum characters to use: ',
+                                maxCharClass: '.maximum-characters',
+                                maxCharMessage: 'Number of characters remaining: ',
+                                minCharsLimit: 50,
+                                maxCharsLimit: 255,
+
+})
+
+
+
+// Text area field for the meta description page
+minimumCharactersToUse(metaDescriptionTextAreaSelector, {
+    minCharClass: '.minimum-characters',
+    minCharMessage: 'Minimum characters to use: ',
+    maxCharClass: '.maximum-characters',
+    maxCharMessage: 'Number of characters remaining: ',
+    minCharsLimit: 10,
+    maxCharsLimit: 500,
+    });
+
+
+// Text area field for the meta description page
+minimumCharactersToUse(warrantyDescriptionTextAreaSelector, {
+    minCharClass: '.minimum-characters',
+    minCharMessage: 'Minimum characters to use: ',
+    maxCharClass: '.maximum-characters',
+    maxCharMessage: 'Number of characters remaining: ',
+    minCharsLimit: 50,
+    maxCharsLimit: 2000,
+    });
+
 
 
 function nextPage(event, pageNumber) {
@@ -36,3 +92,36 @@ function prevPage(event, pageNumber) {
 
 window.nextPage = nextPage;
 window.prevPage = prevPage;
+
+
+async function populateCountrySelect() {
+    const countriesSelectForm = document.querySelector("#countries");
+    try {
+        const filePath      = "../../../../countries.txt";
+        const countriesData = await loadFile(filePath);
+
+        if (countriesData) {
+            const countries = countriesData.split('\n');
+
+            countries.forEach((country) => {
+                const value = country;
+                const text  = country;
+
+                const option = createOption(value, text);
+                countriesSelectForm.appendChild(option);
+            });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function createOption(value, text) {
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = text;
+    return option
+}
+
+
+populateCountrySelect();
--- static/js/pages/pages.js
+++ static/js/pages/pages.js
@@ -3,9 +3,9 @@ const addNewProductPages = {
     1: "basic-product-information.html",
     2: "detailed-description-specs.html",
     3: "pricing-inventory.html",
-    4: "pricing-inventory.html",
+    4: "images-and-media.html",
     5: "shipping-and-delivery.html",
-    6: "SEO-and-meta-information",
+    6: "SEO-and-meta-information.html",
     7: "additonal-information.html",
     8: "review-and-submit.html"
 }
