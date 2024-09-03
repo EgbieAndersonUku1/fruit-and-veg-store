c497c9cf65fed0ed6c0730743d59caca9e34cde9
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-09-03 22:02:28 +0100

    feat: Add Error Handling for Form Validation
    - Integrated custom error handling by importing the `validateElement` function.
    - `validateElement` is responsible for validating form elements and raising an error if they are missing elements.

--- static/js/pages/profile.js
+++ static/js/pages/profile.js
@@ -1,20 +1,23 @@
+import { validateElement } from "../errors/customErrors.js";
+
+
 const billingAddressRadioContainer = document.getElementById("billing-address-radio-buttons");
-const billingAddressElement        = document.getElementById("billing-address-details")
-billingAddressRadioContainer.addEventListener("click", handleRadioButtonClick);
+const billingAddressElement        = document.getElementById("billing-address-details");
 
-
-console.log(billingAddressElement);
+billingAddressRadioContainer?.addEventListener("click", handleRadioButtonClick);
 
 
 function handleRadioButtonClick(e) {
- 
+    
+    validateElement(billingAddressElement, "The billing address element couldn't be found", true);
+
     const value  = e.target.value;
 
     switch(true) {
         case value.toLowerCase() === "no":
             billingAddressElement.classList.remove("d-none");
             break;
-        case value.toLowerCase() === "yes":
+        default:
             billingAddressElement.classList.add("d-none");
             break;
 
